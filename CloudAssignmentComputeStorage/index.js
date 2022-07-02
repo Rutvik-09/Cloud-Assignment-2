947f9ac01b7b7eeca24dcfb1e4d74559a4437db0
Rutvik Pareshbhai Patel <rt994397@dal.ca>
2022-07-02 19:08:57 -0300

    Commiting the assignment

--- CloudAssignmentComputeStorage/index.js
+++ CloudAssignmentComputeStorage/index.js
@@ -1,7 +1,7 @@
 const http = require("http");
-const axios = require("axios");
+// const axios = require("axios");
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 
 const app = require("./app");
 
@@ -12,15 +12,15 @@ const providedURL = "http://52.23.207.11:8081/ ";
 server.listen(port, "0.0.0.0", () => {
   console.log("server is running");
 
-  axios
-    .post(providedURL + "/start", {
-      BannerID: "B00897762",
-      EC2InstancePublicIP: "3.89.20.101",
-    })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  //   axios
+  //     .post(providedURL + "/start", {
+  //       banner: "B00897762",
+  //       ip: " 18.209.47.229",
+  //     })
+  //     .then(function (response) {
+  //       console.log(response);
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
 });
